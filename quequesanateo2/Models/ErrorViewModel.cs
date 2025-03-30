4704752a55c50dfa9debd50d26f8e1c65a5d56e4
danielsct56bm <102069315+danielsct56bm@users.noreply.github.com>
2025-03-30 11:39:24 -0500

    Add files via upload

---
+++ quequesanateo2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace quequesanateo2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
